b55c4675be94eb3a3904f3c71aaf0774f8645f53
sayeedkhannabil <sayeedkhannabil@gmail.com>
2024-12-01 16:23:06 -0600

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import Header from './components/layout/Header/Header'
-import Nav from './components/layout/Nav/Nav'
-import Services from './components/sections/Services/Services'
-import Projects from './components/sections/Project/Project'
-import About from './components/sections/About/About'
-import Contact from './components/sections/Contact/Contact'
-import Footer from './components/layout/Footer/Footer'
-
-
+import React from "react";
+import Header from "./components/layout/Header/Header";
+import Nav from "./components/layout/Nav/Nav";
+import Services from "./components/sections/Services/Services";
+import Projects from "./components/sections/Project/Project";
+import About from "./components/sections/About/About";
+import Contact from "./components/sections/Contact/Contact";
+import Footer from "./components/layout/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='min-h-screen'>
-        <Header/>
-        <Nav/>
-        
-        <main>
-            <Services/>
-            <Projects/>
-            <About/>
-            <Contact/>
-        </main>
+    <div className="min-h-screen">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
+      <Nav />
 
-        <Footer/>
+      <main>
+        <Services />
+        <Projects />
+        <About />
+        <Contact />
+      </main>
+
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
