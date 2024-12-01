ac4fc08a889c727d7d54217265e41fed0f10a556
sayeedkhannabil <sayeedkhannabil@gmail.com>
2024-12-01 16:57:56 -0600

    Header Initial Done

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import Footer from "./components/layout/Footer/Footer";
 const App = () => {
   return (
     <div className="min-h-screen">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Header />
       <Nav />
 
--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,10 +1,122 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import logo from "../../../assets/header_logo.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <header className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-24 items-center justify-between">
+            <div className="md:flex md:items-center md:gap-12">
+              <a className="block text-teal-600" href="#">
+                <img src={logo} alt="Meaningful text" className="max-h-20 w-auto" />
+              </a>
+            </div>
 
-export default Header
+            <div className="hidden md:block">
+              <nav aria-label="Global">
+                <ul className="flex items-center gap-8 text-base">
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="#"
+                    >
+                      About
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="#"
+                    >
+                      Careers
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="#"
+                    >
+                      History
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="#"
+                    >
+                      Services
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="#"
+                    >
+                      Projects
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      className="text-gray-500 transition hover:text-gray-500/75"
+                      href="#"
+                    >
+                      Blog
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div className="flex items-center gap-6">
+              <div className="sm:flex sm:gap-4">
+                <a
+                  className="rounded-md bg-teal-600 px-6 py-3 text-base font-medium text-white shadow"
+                  href="#"
+                >
+                  Login
+                </a>
+
+                <div className="hidden sm:flex">
+                  <a
+                    className="rounded-md bg-gray-100 px-6 py-3 text-base font-medium text-teal-600"
+                    href="#"
+                  >
+                    Register
+                  </a>
+                </div>
+              </div>
+
+              <div className="block md:hidden">
+                <button className="rounded bg-gray-100 p-3 text-gray-600 transition hover:text-gray-600/75">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
