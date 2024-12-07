4e628e616c0e98fc1d5334b5b761a40ad7f1d468
sayeedkhannabil <sayeedkhannabil@gmail.com>
2024-12-07 01:57:45 -0600

    nav experiment

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.css";
-import logo from "../../../assets/header_logo.png"
+import logo from "../../../assets/header_logo.png";
 
 const Header = () => {
   return (
--- src/components/layout/Nav/Nav.jsx
+++ src/components/layout/Nav/Nav.jsx
@@ -1,58 +1,67 @@
-import React from 'react'
-import './nav.css'
+import React from "react";
+import logo from "../../../assets/nav_picture.png";
 
 const Nav = () => {
   return (
     <div>
-{/*
+      {/*
   Heads up! 👋
 
   This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
 */}
 
-<section
-  className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
->
-  <div
-    className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
-  ></div>
+      <section className="relative">
+        {/* Background div */}
+        <div
+          style={{
+            backgroundImage: `url(${logo})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        ></div>
+        <div className="absolute inset-0 bg-black/40 backdrop-blur-smabsolute inset-0 bg-black/40 backdrop-blur-sm"></div>
 
-  <div
-    className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
-  >
-    <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-      <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
-        If You Can Dream It
+        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+          <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+            <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
+              If You Can Dream It
+              <strong className="block font-extrabold text-rose-500">
+                {" "}
+                We Can Build it.{" "}
+              </strong>
+            </h1>
 
-        <strong className="block font-extrabold text-rose-500"> We Can Build it. </strong>
-      </h1>
+            <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+              illo tenetur fuga ducimus numquam ea!
+            </p>
 
-      <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-        numquam ea!
-      </p>
+            <div className="mt-8 flex flex-wrap gap-4 text-center">
+              <a
+                href="#"
+                className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+              >
+                Get Started
+              </a>
 
-      <div className="mt-8 flex flex-wrap gap-4 text-center">
-        <a
-          href="#"
-          className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
-        >
-          Get Started
-        </a>
-
-        <a
-          href="#"
-          className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
-        >
-          Learn More
-        </a>
-      </div>
+              <a
+                href="#"
+                className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  </div>
-</section>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
