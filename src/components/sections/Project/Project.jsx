71d3c807140c5e19ca19bb86522ab4eb29999fd2
Sheikh Shafayet Hossain <sadithossain@gmail.com>
2024-12-08 16:47:23 -0600

    projects

--- src/components/sections/Project/Project.jsx
+++ src/components/sections/Project/Project.jsx
@@ -50,37 +50,43 @@ const callouts = [
 const Projects = () => {
   return (
     <div className="bg-gray-100">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-          <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
+        <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32 ">
+          <h2 className="text-2xl font-bold text-gray-900 text-center"> OUR WORK </h2>
 
           <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
             {callouts.map((callout) => (
-              <div key={callout.name} className="group relative">
+              <div
+                key={callout.name}
+                className="group relative rounded-lg overflow-hidden"
+              >
                 <img
                   alt={callout.imageAlt}
                   src={callout.imageSrc}
                   className="w-full rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-2/1 lg:aspect-square"
                 />
-                <h3 className="mt-6 text-sm text-gray-500">
+                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity">
+                  <p className="text-sm text-white p-4 text-center">
+                    {callout.description}
+                  </p>
+                </div>
+                <h3 className="mt-6 text-sm text-gray-500 text-center">
                   <a href={callout.href}>
                     <span className="absolute inset-0" />
                     {callout.name}
                   </a>
                 </h3>
-                {/* <p className="text-base font-semibold text-gray-900">{callout.description}</p> */}
               </div>
             ))}
           </div>
           <div className="mt-8 flex flex-wrap gap-4 text-center justify-center">
-              <a
-                href="#"
-                className="block w-full rounded bg-[#60AD02] px-12 py-3 text-sm font-medium text-white shadow hover:bg-[#068510] focus:outline-none focus:ring active:bg-[#068510] sm:w-auto"
-              >
-                View More
-              </a>
-            </div>
-
+            <a
+              href="#"
+              className="block w-full rounded bg-[#60AD02] px-12 py-3 text-sm font-medium text-white shadow hover:bg-[#068510] focus:outline-none focus:ring active:bg-[#068510] sm:w-auto"
+            >
+              View More
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -88,3 +94,4 @@ const Projects = () => {
 };
 
 export default Projects;
+
